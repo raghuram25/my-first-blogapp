55406ea893775b1c6846ae960eae988961837b30
Raghuram25 <raghuram39797@gmail.com>
2022-06-27 22:43:51 +0530

    Modified templates as dataorder back

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from .models import Post  #Post is a class which we created in models.py which h
 from django.utils import timezone #To display our blogs based on published date
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
