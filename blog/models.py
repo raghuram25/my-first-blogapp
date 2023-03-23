4951eaf3ced2c7b425315044bdc83fcba6a5ce72
Raghuram25 <raghuram39797@gmail.com>
2023-03-23 18:50:51 +0530

    Changed icon name to Myblogsapp.

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-class Post(models.Model):                   #Post Model
+class Post(models.Model):                   #Post Model(It is like a schema where we declare the columns name(variables here) and datatypes)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    title = models.CharField(max_length=200)            # is equal to  ---> sql/    text varchar(200);
+    text = models.TextField()               
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+# Above we can see the while defining the Class 'Post' we gave model.Model in () and below all the datatype declarations are in the form "model.xxxx"
     def publish(self):
         self.published_date = timezone.now()
         self.save()
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404 #incase there are no posts we get this error (error handling)
-from .models import Post  #Post is a class which we created in models.py which has datatype declared
+from .models import Post                               #Post is a class which we created in models.py which has datatype declared
 from django.utils import timezone
-from .forms import PostForm #To display our blogs based on published date
-                                                                                                # Create your views here.
+from .forms import PostForm                            #To display our blogs based on published 
+
+# Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -15,7 +16,7 @@ def post_new(request):
     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-from django.shortcuts import redirect #after getting data from forms and on save it will redirect to post_detail view(original page)
+from django.shortcuts import redirect                      #after getting data from forms and on click of save , it will redirect to post_detail view(original page)
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -29,7 +30,12 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-def post_edit(request, pk):                            #postedit feature for existing posts
+#The above two "post_new" functions , the first one is called upon click of 'new post' button. So if we try to click Save without filling any data
+#it is again calling the form = Postform() in the else block...So only same page is reloaded. if post is valid ...that means if we type something
+#according to Validity it will be save (post.save())
+
+
+def post_edit(request, pk):                                        #postedit feature for existing posts
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
